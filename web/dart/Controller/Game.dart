156fbfb7513362cd316672eff6d79a967309367a
sticky4loop <christoffer-ekblom@ChristoffersMBP.lan>
2016-10-18 20:19:07 +0200

    Refactoring

--- web/dart/Controller/Game.dart
+++ web/dart/Controller/Game.dart
@@ -3,18 +3,17 @@ import '../View/Renderer.dart';
 import '../View/Content.dart';
 
 abstract class Game {
-  Renderer _renderer;
   Content _contentManager;
 
   Content get ContentManager => _contentManager;
 
   Game() {
     double startTime = 0.0;
-
-    _renderer = new Renderer();
+    Element body = document.body;
+    Renderer renderer = new Renderer();
     _contentManager = new Content();
 
-    ContentManager.load(document.body, _renderer.GetRenderer.domElement);
+    ContentManager.load(body, renderer.GetRenderer.domElement);
 
     initialize();
     loadContent();
